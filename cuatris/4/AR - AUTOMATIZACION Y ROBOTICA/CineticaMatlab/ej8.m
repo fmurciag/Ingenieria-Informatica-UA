8bc5848355651e4b4a400c24905fabfe84b1a1b1
fran <fmurciag@gmail.com>
2022-05-10 23:59:45 +0200

    ar

--- cuatris/4/AR - AUTOMATIZACION Y ROBOTICA/CineticaMatlab/ej8.m
+++ cuatris/4/AR - AUTOMATIZACION Y ROBOTICA/CineticaMatlab/ej8.m
@@ -6,8 +6,6 @@ qs = [0, pi/4, pi/2, 0, -pi/4, 0];
 q1 = [0, pi/4, pi/4, 0, pi/2, 0];
 q2 = [deg2rad(20), pi/2, pi/4, deg2rad(-22.5), pi/3, 0];
 
-%% seteamos gravedad Lunar
-robot.gravity = [0 0 1.62];
 
 %% con peso de 3kg
 robot.payload(3, [0, 0, 0]);
@@ -19,8 +17,6 @@ fprintf("Gravedad:")
 disp(robot.gravload(qs));
 fprintf("Inercia:")
 disp(robot.itorque(qs,[1 0 0 0 0 0]));
-fprintf("Coriolis:\n")
-disp(robot.coriolis(qs, [0 pi 0 0 0 0]));
 
 %%posicion q1
 fprintf("\n===posicion q1====\n:")
@@ -28,8 +24,6 @@ fprintf("Gravedad:")
 disp(robot.gravload(q1));
 fprintf("Inercia:")
 disp(robot.itorque(q1,[1 0 0 0 0 0]));
-fprintf("Coriolis:\n")
-disp(robot.coriolis(q1, [0 pi 0 0 0 0]));
 
 %%posicion q2
 fprintf("\n===posicion q2====\n:")
@@ -37,8 +31,6 @@ fprintf("Gravedad:")
 disp(robot.gravload(q2));
 fprintf("Inercia:")
 disp(robot.itorque(q2,[1 0 0 0 0 0]));
-fprintf("Coriolis:\n")
-disp(robot.coriolis(q2, [0 pi 0 0 0 0]));
 
 %% con peso de 3kg separado en el eje X 0.3m
 robot.payload(3, [0.3, 0, 0.1]);
@@ -50,8 +42,6 @@ fprintf("Gravedad:")
 disp(robot.gravload(qs));
 fprintf("Inercia:")
 disp(robot.itorque(qs,[1 0 0 0 0 0]));
-fprintf("Coriolis:\n")
-disp(robot.coriolis(qs, [0 pi 0 0 0 0]));
 
 %%posicion q1
 fprintf("\n===posicion q1====\n:")
@@ -59,8 +49,6 @@ fprintf("Gravedad:")
 disp(robot.gravload(q1));
 fprintf("Inercia:")
 disp(robot.itorque(q1,[1 0 0 0 0 0]));
-fprintf("Coriolis:\n")
-disp(robot.coriolis(q1, [0 pi 0 0 0 0]));
 
 %%posicion q2
 fprintf("\n===posicion q2====\n:")
@@ -68,7 +56,4 @@ fprintf("Gravedad:")
 disp(robot.gravload(q2));
 fprintf("Inercia:")
 disp(robot.itorque(q2,[1 0 0 0 0 0]));
-fprintf("Coriolis:\n")
-disp(robot.coriolis(q2, [0 pi 0 0 0 0]));
-
 
